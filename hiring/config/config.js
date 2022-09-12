96ef39615967f49a0389f64f09b7b49f552461e1
Firos K <firos.k@donyati.com>
2022-09-12 22:59:45 +0530

    [Updated] - Form link changed

--- hiring/config/config.js
+++ hiring/config/config.js
@@ -1,6 +1,6 @@
 // Change the resume upload form link
 // whenever required
-const formLink = "https://forms.gle/kjXzb3vHjFekmknr8";
+const formLink = "https://forms.gle/e7VGbzq1riPkr64a6";
 
 // Attaching the link to button
 jQuery(document).ready(function($) {
