7f1fa1448225ae1417d40a8b12ec5bb11373ce1b
Firos K <firos.k@donyati.com>
2022-09-14 13:36:14 +0530

    [Added] - Buttons for fresher and experienced

--- hiring/config/config.js
+++ hiring/config/config.js
@@ -3,6 +3,6 @@
 const formLink = "https://forms.gle/e7VGbzq1riPkr64a6";
 
 // Attaching the link to button
-jQuery(document).ready(function($) {
-    $("#upload-resume-button").attr("href", formLink)
+jQuery(document).ready(function ($) {
+    $("#upload-resume-fresher").attr("href", formLink)
 });
